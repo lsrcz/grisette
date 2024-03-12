98ac2bc9f52d7f65fc08621c8955f560491eae72
Sirui Lu <siruilu@cs.washington.edu>
2024-03-12 14:06:03 -0700

    :sparkles: Add mrg variants for Data.Traversable functions

--- test/Grisette/Lib/Data/TraversableTests.hs
+++ test/Grisette/Lib/Data/TraversableTests.hs
@@ -5,7 +5,6 @@ module Grisette.Lib.Data.TraversableTests (traversableFunctionTests) where
 import Control.Monad.Except
   ( ExceptT (ExceptT),
     MonadError (throwError),
-    runExceptT,
   )
 import Grisette.Core.Control.Monad.UnionM (UnionM, mergePropagatedIf)
 import Grisette.Core.Data.Class.SimpleMergeable
@@ -14,15 +13,24 @@ import Grisette.Core.Data.Class.SimpleMergeable
 import Grisette.Core.Data.Class.TryMerge
   ( mrgSingle,
   )
+import Grisette.Lib.Control.Monad (mrgReturn)
 import Grisette.Lib.Data.Traversable
   ( mrgFor,
+    mrgForAccumM,
     mrgForM,
+    mrgMapAccumM,
     mrgMapM,
     mrgSequence,
     mrgSequenceA,
     mrgTraverse,
   )
-import Test.Framework (Test, testGroup)
+import Grisette.TestUtil.NoMerge (oneNotMerged)
+import Test.Framework
+  ( Test,
+    TestOptions' (topt_timeout),
+    plusTestOptions,
+    testGroup,
+  )
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit ((@?=))
 
@@ -30,84 +38,137 @@ traversableFunctionTests :: Test
 traversableFunctionTests =
   testGroup
     "Traversable"
-    [ testCase "mrgTraverse" $ do
-        runExceptT
-          ( mrgTraverse
-              (\(c, d, x, y, z) -> ExceptT $ mergePropagatedIf c (return $ Left x) (mergePropagatedIf d (return $ Right y) (return $ Right z)))
-              [("a", "c", 3, 4, 5), ("b", "d", 2, 3, 6)] ::
-              ExceptT Integer UnionM [Integer]
-          )
-          @?= runExceptT
-            ( do
-                a <- mrgIf "a" (throwError 3) (mrgIf "c" (return 4) (return 5))
-                b <- mrgIf "b" (throwError 2) (mrgIf "d" (return 3) (return 6))
-                mrgSingle [a, b]
-            ),
-      testCase "mrgSequenceA" $ do
-        runExceptT
-          ( mrgSequenceA
-              [ ExceptT $ mergePropagatedIf "a" (return $ Left 3) (mergePropagatedIf "c" (return $ Right 4) (return $ Right 5)),
-                ExceptT $ mergePropagatedIf "b" (return $ Left 2) (mergePropagatedIf "d" (return $ Right 3) (return $ Right 6))
-              ] ::
-              ExceptT Integer UnionM [Integer]
-          )
-          @?= runExceptT
-            ( do
-                a <- mrgIf "a" (throwError 3) (mrgIf "c" (return 4) (return 5))
-                b <- mrgIf "b" (throwError 2) (mrgIf "d" (return 3) (return 6))
-                mrgSingle [a, b]
-            ),
-      testCase "mrgMapM" $ do
-        runExceptT
-          ( mrgMapM
-              (\(c, d, x, y, z) -> ExceptT $ mergePropagatedIf c (return $ Left x) (mergePropagatedIf d (return $ Right y) (return $ Right z)))
-              [("a", "c", 3, 4, 5), ("b", "d", 2, 3, 6)] ::
-              ExceptT Integer UnionM [Integer]
-          )
-          @?= runExceptT
-            ( do
-                a <- mrgIf "a" (throwError 3) (mrgIf "c" (return 4) (return 5))
-                b <- mrgIf "b" (throwError 2) (mrgIf "d" (return 3) (return 6))
-                mrgSingle [a, b]
-            ),
-      testCase "mrgSequence" $ do
-        runExceptT
-          ( mrgSequence
-              [ ExceptT $ mergePropagatedIf "a" (return $ Left 3) (mergePropagatedIf "c" (return $ Right 4) (return $ Right 5)),
-                ExceptT $ mergePropagatedIf "b" (return $ Left 2) (mergePropagatedIf "d" (return $ Right 3) (return $ Right 6))
-              ] ::
-              ExceptT Integer UnionM [Integer]
-          )
-          @?= runExceptT
-            ( do
-                a <- mrgIf "a" (throwError 3) (mrgIf "c" (return 4) (return 5))
-                b <- mrgIf "b" (throwError 2) (mrgIf "d" (return 3) (return 6))
-                mrgSingle [a, b]
-            ),
-      testCase "mrgFor" $ do
-        runExceptT
-          ( mrgFor
-              [("a", "c", 3, 4, 5), ("b", "d", 2, 3, 6)]
-              (\(c, d, x, y, z) -> ExceptT $ mergePropagatedIf c (return $ Left x) (mergePropagatedIf d (return $ Right y) (return $ Right z))) ::
-              ExceptT Integer UnionM [Integer]
-          )
-          @?= runExceptT
-            ( do
-                a <- mrgIf "a" (throwError 3) (mrgIf "c" (return 4) (return 5))
-                b <- mrgIf "b" (throwError 2) (mrgIf "d" (return 3) (return 6))
-                mrgSingle [a, b]
-            ),
-      testCase "mrgForM" $ do
-        runExceptT
-          ( mrgForM
-              [("a", "c", 3, 4, 5), ("b", "d", 2, 3, 6)]
-              (\(c, d, x, y, z) -> ExceptT $ mergePropagatedIf c (return $ Left x) (mergePropagatedIf d (return $ Right y) (return $ Right z))) ::
-              ExceptT Integer UnionM [Integer]
-          )
-          @?= runExceptT
-            ( do
-                a <- mrgIf "a" (throwError 3) (mrgIf "c" (return 4) (return 5))
-                b <- mrgIf "b" (throwError 2) (mrgIf "d" (return 3) (return 6))
-                mrgSingle [a, b]
-            )
+    [ plusTestOptions (mempty {topt_timeout = Just (Just 1000000)}) $
+        testGroup "mrgTraverse, mrgMapM, mrgFor, mrgForM" $ do
+          (name, func0, func1) <-
+            [ ("mrgTraverse", mrgTraverse, mrgTraverse),
+              ("mrgMapM", mrgMapM, mrgMapM),
+              ("mrgFor", flip mrgFor, flip mrgFor),
+              ("mrgForM", flip mrgForM, flip mrgForM)
+              ]
+          return $
+            testGroup
+              name
+              [ testCase "semantics" $ do
+                  let actual =
+                        func0
+                          ( \(c, d, x, y, z) ->
+                              ExceptT $
+                                mergePropagatedIf
+                                  c
+                                  (return $ Left x)
+                                  ( mergePropagatedIf
+                                      d
+                                      (return $ Right y)
+                                      (return $ Right z)
+                                  )
+                          )
+                          [("a", "c", 3, 4, 5), ("b", "d", 2, 3, 6)] ::
+                          ExceptT Integer UnionM [Integer]
+                  let expected = do
+                        a <-
+                          mrgIf
+                            "a"
+                            (throwError 3)
+                            (mrgIf "c" (return 4) (return 5))
+                        b <-
+                          mrgIf
+                            "b"
+                            (throwError 2)
+                            (mrgIf "d" (return 3) (return 6))
+                        mrgSingle [a, b]
+                  actual @?= expected,
+                testCase "merge intermediate" $ do
+                  let actual = func1 (const oneNotMerged) [1 .. 1000]
+                  let expected = mrgReturn $ replicate 1000 1
+                  actual @?= expected
+              ],
+      plusTestOptions (mempty {topt_timeout = Just (Just 1000000)}) $
+        testGroup "mrgSequenceA, mrgSequence" $ do
+          (name, func0, func1) <-
+            [ ("mrgSequenceA", mrgSequenceA, mrgSequenceA),
+              ("mrgSequence", mrgSequence, mrgSequence)
+              ]
+          return $
+            testGroup
+              name
+              [ testCase "semantics" $ do
+                  let actual =
+                        func0
+                          [ ExceptT $
+                              mergePropagatedIf
+                                "a"
+                                (return $ Left 3)
+                                ( mergePropagatedIf
+                                    "c"
+                                    (return $ Right 4)
+                                    (return $ Right 5)
+                                ),
+                            ExceptT $
+                              mergePropagatedIf
+                                "b"
+                                (return $ Left 2)
+                                ( mergePropagatedIf
+                                    "d"
+                                    (return $ Right 3)
+                                    (return $ Right 6)
+                                )
+                          ] ::
+                          ExceptT Integer UnionM [Integer]
+                  let expected = do
+                        a <-
+                          mrgIf
+                            "a"
+                            (throwError 3)
+                            (mrgIf "c" (return 4) (return 5))
+                        b <-
+                          mrgIf
+                            "b"
+                            (throwError 2)
+                            (mrgIf "d" (return 3) (return 6))
+                        mrgSingle [a, b]
+                  actual @?= expected,
+                testCase "merge intermediate" $ do
+                  let actual = func1 (replicate 1000 oneNotMerged)
+                  let expected = mrgReturn $ replicate 1000 1
+                  actual @?= expected
+              ],
+      plusTestOptions (mempty {topt_timeout = Just (Just 1000000)}) $
+        testGroup "mrgMapAccumM, mrgForAccumM" $ do
+          (name, func0, func1) <-
+            [ ("mrgMapAccumM", mrgMapAccumM, mrgMapAccumM),
+              ( "mrgForAccumM",
+                \f s t -> mrgForAccumM s t f,
+                \f s t -> mrgForAccumM s t f
+              )
+              ]
+          return $
+            testGroup
+              name
+              [ testCase "semantics" $ do
+                  let actual =
+                        func0 (\a b -> mrgSingle (a + b, a - b)) 0 [1 .. 1000]
+                  let expected =
+                        mrgReturn
+                          ( 500500,
+                            [(i - 1) * i `div` 2 - i | i <- [1 .. 1000]]
+                          ) ::
+                          UnionM (Integer, [Integer])
+                  actual @?= expected,
+                testCase "merge intermediate" $ do
+                  let actual =
+                        func1
+                          ( \_ _ ->
+                              mergePropagatedIf
+                                "a"
+                                (return (1, 1))
+                                (return (1, 1))
+                          )
+                          0
+                          [1 .. 1000]
+                  let expected =
+                        mrgReturn (1, replicate 1000 1) ::
+                          UnionM (Integer, [Integer])
+                  actual @?= expected
+              ]
     ]
