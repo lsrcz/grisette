d9d7a1bcb2eea671e932483b5a50ae98b1cb7082
Sirui Lu <siruilu@cs.washington.edu>
2024-06-24 01:50:41 -0700

    :bug: Fix build on GHC 9.0.2

--- src/Grisette/Unified/Internal/BaseMonad.hs
+++ src/Grisette/Unified/Internal/BaseMonad.hs
@@ -2,6 +2,14 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 
+-- |
+-- Module      :   Grisette.Unified.Internal.BaseMonad
+-- Copyright   :   (c) Sirui Lu 2024
+-- License     :   BSD-3-Clause (see the LICENSE file)
+--
+-- Maintainer  :   siruilu@cs.washington.edu
+-- Stability   :   Experimental
+-- Portability :   GHC only
 module Grisette.Unified.Internal.BaseMonad
   ( BaseMonad,
   )
@@ -12,6 +20,9 @@ import Data.Kind (Type)
 import Grisette.Internal.Core.Control.Monad.UnionM (UnionM)
 import Grisette.Unified.Internal.EvaluationMode (EvaluationMode (Con, Sym))
 
+-- | A type family that specifies the base monad for the evaluation mode.
+--
+-- Resolves to 'Identity' for `Con` mode, and 'UnionM' for `Sym` mode.
 type family
   BaseMonad (mode :: EvaluationMode) =
     (r :: Type -> Type) | r -> mode
