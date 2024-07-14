b64409d6ae76f847f4b2ff445f4f9da2a1372ddd
Sirui Lu <siruilu@cs.washington.edu>
2024-07-13 22:50:25 -0700

    :sparkles: Add existsFresh and forallFresh

--- test/Grisette/SymPrim/QuantifierTests.hs
+++ test/Grisette/SymPrim/QuantifierTests.hs
@@ -1,24 +1,28 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Grisette.SymPrim.QuantifierTests (quantifierTests) where
 
 import Grisette
   ( Function ((#)),
+    GenSymSimple (simpleFresh),
     LogicalOp (symImplies),
     ModelOps (isEmptyModel),
+    SimpleListSpec (SimpleListSpec),
     SymEq ((.==)),
     SymOrd ((.>)),
+    runFresh,
     solve,
     z3,
   )
-import Grisette.Internal.SymPrim.Quantifier (existsSym, forallSym)
+import Grisette.Internal.SymPrim.Quantifier (existsFresh, existsSym, forallFresh, forallSym)
 import Grisette.Internal.SymPrim.SymInteger (SymInteger)
 import Grisette.SymPrim (type (=~>))
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
-import Test.HUnit (assertBool)
+import Test.HUnit (assertBool, (@?=))
 
 #if MIN_VERSION_sbv(10,1,0)
 sbvVersionCheck :: Test -> Test
@@ -59,5 +63,15 @@ quantifierTests =
                   (x .== y) `symImplies` ((f # x) .== (f # y))
           case r of
             Left err -> error $ show err
-            Right _ -> return ()
+            Right _ -> return (),
+        testCase "fresh" $ do
+          let x = flip runFresh "x" $
+                forallFresh (SimpleListSpec 2 ()) $ \(l :: [SymInteger]) ->
+                  existsFresh (SimpleListSpec 2 ()) $ \(r :: [SymInteger]) ->
+                    return $ l .== r
+          let r = flip runFresh "x" $ do
+                l :: [SymInteger] <- simpleFresh (SimpleListSpec 2 ())
+                r <- simpleFresh (SimpleListSpec 2 ())
+                return $ forallSym l $ existsSym r $ l .== r
+          x @?= r
       ]
