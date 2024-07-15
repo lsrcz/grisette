92d616febe4943a86b023ab540b0a4bba8b885d3
Sirui Lu <siruilu@cs.washington.edu>
2024-07-15 04:28:01 -0700

    :sparkles: Add FractionalOr and LogBaseOr

--- src/Grisette/Internal/Core/Data/Class/SafeLogBase.hs
+++ src/Grisette/Internal/Core/Data/Class/SafeLogBase.hs
@@ -10,11 +10,17 @@
 -- Maintainer  :   siruilu@cs.washington.edu
 -- Stability   :   Experimental
 -- Portability :   GHC only
-module Grisette.Internal.Core.Data.Class.SafeLogBase (SafeLogBase (..)) where
+module Grisette.Internal.Core.Data.Class.SafeLogBase
+  ( SafeLogBase (..),
+    LogBaseOr (..),
+    logBaseOrZero,
+  )
+where
 
 import Control.Exception (ArithException (RatioZeroDenominator))
 import Control.Monad.Error.Class (MonadError (throwError))
 import Grisette.Internal.Core.Control.Monad.Class.Union (MonadUnion)
+import Grisette.Internal.Core.Data.Class.ITEOp (ITEOp (symIte))
 import Grisette.Internal.Core.Data.Class.Mergeable (Mergeable)
 import Grisette.Internal.Core.Data.Class.SimpleMergeable (mrgIf)
 import Grisette.Internal.Core.Data.Class.SymEq (SymEq ((.==)))
@@ -32,6 +38,19 @@ import Grisette.Internal.SymPrim.SymAlgReal (SymAlgReal (SymAlgReal))
 -- >>> import Control.Monad.Except
 -- >>> import Control.Exception
 
+-- | Safe 'logBase' with default values returned on exception.
+class LogBaseOr a where
+  -- | Safe 'logBase' with default values returned on exception.
+  --
+  -- >>> logBaseOr "d" "base" "val" :: SymAlgReal
+  -- (ite (= base 1.0) d (fdiv (log val) (log base)))
+  logBaseOr :: a -> a -> a -> a
+
+-- | Safe 'logBase' with 0 returned on exception.
+logBaseOrZero :: (LogBaseOr a, Num a) => a -> a -> a
+logBaseOrZero l = logBaseOr (l - l) l
+{-# INLINE logBaseOrZero #-}
+
 -- | Safe 'logBase' with monadic error handling in multi-path execution.
 -- These procedures throw an exception when the base is 1.
 -- The result should be able to handle errors with `MonadError`.
@@ -44,7 +63,14 @@ class (MonadError e m, TryMerge m, Mergeable a) => SafeLogBase e a m where
   safeLogBase = undefined
   {-# INLINE safeLogBase #-}
 
-  {-# MINIMAL safeLogBase #-}
+instance LogBaseOr SymAlgReal where
+  logBaseOr d base@(SymAlgReal baset) (SymAlgReal at) =
+    symIte (base .== 1) d $
+      SymAlgReal $
+        pevalFdivTerm
+          (pevalFloatingUnaryTerm FloatingLog at)
+          (pevalFloatingUnaryTerm FloatingLog baset)
+  {-# INLINE logBaseOr #-}
 
 instance
   (MonadError ArithException m, MonadUnion m) =>
