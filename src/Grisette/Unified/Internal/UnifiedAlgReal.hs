150e17929cf87c4f91007b0bf3207b97dd910af4
Sirui Lu <siruilu@cs.washington.edu>
2024-08-02 00:56:35 -0700

    :sparkles: Add UnifiedSafeFdiv

--- src/Grisette/Unified/Internal/UnifiedAlgReal.hs
+++ src/Grisette/Unified/Internal/UnifiedAlgReal.hs
@@ -2,8 +2,10 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE TypeOperators #-}
 
 -- |
 -- Module      :   Grisette.Unified.Internal.UnifiedAlgReal
@@ -13,12 +15,23 @@
 -- Maintainer  :   siruilu@cs.washington.edu
 -- Stability   :   Experimental
 -- Portability :   GHC only
-module Grisette.Unified.Internal.UnifiedAlgReal (UnifiedAlgReal) where
+module Grisette.Unified.Internal.UnifiedAlgReal
+  ( UnifiedAlgReal,
+    GetAlgReal,
+  )
+where
 
+import Control.Exception (ArithException)
+import Control.Monad.Error.Class (MonadError)
 import Grisette.Internal.Core.Data.Class.SafeFdiv (FdivOr)
 import Grisette.Internal.SymPrim.AlgReal (AlgReal)
 import Grisette.Internal.SymPrim.SymAlgReal (SymAlgReal)
-import Grisette.Unified.Internal.BaseConstraint (BasicGrisetteType, ConSymConversion)
+import Grisette.Unified.Internal.BaseConstraint
+  ( BasicGrisetteType,
+    ConSymConversion,
+  )
+import Grisette.Unified.Internal.Class.UnifiedSafeFdiv (UnifiedSafeFdiv)
+import Grisette.Unified.Internal.Class.UnifiedSimpleMergeable (UnifiedBranching)
 import Grisette.Unified.Internal.EvalModeTag (EvalModeTag (Con, Sym))
 import Grisette.Unified.Internal.UnifiedConstraint (UnifiedPrimitive)
 
@@ -28,11 +41,17 @@ class
     Num (GetAlgReal mode),
     Fractional (GetAlgReal mode),
     UnifiedPrimitive mode (GetAlgReal mode),
-    FdivOr (GetAlgReal mode)
+    FdivOr (GetAlgReal mode),
+    forall m.
+    (UnifiedBranching mode m, MonadError ArithException m) =>
+    UnifiedSafeFdiv mode ArithException r m,
+    r ~ GetAlgReal mode
   ) =>
-  UnifiedAlgRealImpl (mode :: EvalModeTag) a
-    | mode -> a
+  UnifiedAlgRealImpl (mode :: EvalModeTag) r
+    | mode -> r
   where
+  -- | 'Floating', 'Grisette.LogBaseOr' and 'Grisette.SafeLogBase' for
+  -- 'SymAlgReal' are not provided as they are not available for 'AlgReal'.
   type GetAlgReal mode = real | real -> mode
 
 instance UnifiedAlgRealImpl 'Con AlgReal where
@@ -41,6 +60,7 @@ instance UnifiedAlgRealImpl 'Con AlgReal where
 instance UnifiedAlgRealImpl 'Sym SymAlgReal where
   type GetAlgReal 'Sym = SymAlgReal
 
+-- | Evaluation mode with unified 'AlgReal' type.
 class
   (UnifiedAlgRealImpl mode (GetAlgReal mode)) =>
   UnifiedAlgReal (mode :: EvalModeTag)
