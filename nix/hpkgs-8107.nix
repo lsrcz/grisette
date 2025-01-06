ab11ff94ee5087efc1c060ea740ef895eb4b0a6f
Sirui Lu <siruilu@cs.washington.edu>
2025-01-05 18:52:58 -0800

    :recycle: Move nix files out of the repo

--- nix/hpkgs-8107.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }:
-with pkgs.haskell.lib;
-(import ./hpkgs-base.nix { inherit pkgs; ghcVersion = "8107"; }).extend (
-  hfinal: hprev: {
-    sbv = dontCheck (hfinal.callHackage "sbv" "9.2" { });
-  }
-)
