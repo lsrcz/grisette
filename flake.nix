c9c04f5f28fd55dd4a533268f18be8aa0ed11fa2
Sirui Lu <siruilu@cs.washington.edu>
2025-01-05 19:35:30 -0800

    :wrench: Update flake.nix and CI configurations

--- flake.nix
+++ flake.nix
@@ -30,44 +30,16 @@
                 inherit pkgs hfinal hprev ghcVersion;
               };
             {
-              grisette = (pkgs.haskell.lib.overrideCabal
-                (hfinal.callCabal2nix "grisette" ./. { })
-                (drv: {
-                  testToolDepends = byGhcVersion {
-                    "8107" = [ pkgs.z3 ];
-                    "902" = [ pkgs.z3 ];
-                    default = [ pkgs.z3 pkgs.bitwuzla ];
-                  };
-                } // (if ci then {
-                  configureFlags = [
-                    "--flags=-optimize"
-                    "--enable-coverage"
-                    "--enable-library-coverage"
-                  ];
-                  postCheck = ''
-                    mkdir -p $out/test-report
-                    cp test-report.xml $out/test-report/test-report.xml
-                  '' + (if ghcVersion == "9101" then ''
-                    mkdir -p $out/mix
-                    cp dist/ $out/dist -r
-                    cp dist/build/extra-compilation-artifacts/hpc/vanilla/mix/* $out/mix -r
-                    cp dist/build/spec/spec-tmp/extra-compilation-artifacts/hpc/vanilla/mix/* $out/mix -r
-                    cp dist/build/doctest/doctest-tmp/extra-compilation-artifacts/hpc/vanilla/mix/* $out/mix -r
-                    mkdir -p $out/tix
-                    find dist/hpc/vanilla/tix -name '*.tix' | xargs -I {} cp {} $out/tix -r
-                  '' else "");
-                  haddockFlags = [
-                    "--html-location='https://hackage.haskell.org/package/\$pkg-\$version/docs'"
-                  ];
-                  postHaddock = ''
-                    mkdir -p $out
-                    cp -r dist/doc/ $out/haddock -r
-                  '';
-                } else { }))).overrideAttrs (if ci then {
-                checkFlags = [
-                  "--test-options=--jxml=test-report.xml"
-                ];
-              } else { });
+              grisette = envLib.setCIOptions {
+                inherit pkgs ghcVersion hfinal ci;
+                extraTestToolDepends = byGhcVersion {
+                  "8107" = [ pkgs.z3 ];
+                  "902" = [ pkgs.z3 ];
+                  default = [ pkgs.z3 pkgs.bitwuzla ];
+                };
+                package = hfinal.callCabal2nix "grisette" ./. { };
+                mixDirs = [ "" "spec/spec-tmp" "doctest/doctest-tmp" ];
+              };
             });
 
         devShellWithVersion =
@@ -84,20 +56,16 @@
             ] ++ (if bitwuzla then [ pkgs.bitwuzla ] else [ ]);
           };
 
-        plainOutputs = pkgs.lib.foldl'
-          (acc: ghcVersion:
-            pkgs.lib.recursiveUpdate acc
-              {
-                devShells.${ghcVersion} =
-                  devShellWithVersion { ghcVersion = ghcVersion; };
-                packages.grisette.${ghcVersion} =
-                  (hPkgs { ghcVersion = ghcVersion; ci = false; }).grisette;
-                packages.grisette."${ghcVersion}-ci" =
-                  (hPkgs { ghcVersion = ghcVersion; ci = true; }).grisette;
-              })
-          { } [ "8107" "902" "928" "948" "966" "984" "9101" "9121" ];
+        plainOutputs = envLib.defaultOutputs {
+          inherit pkgs devShellWithVersion;
+          haskellPackagesWithCiFlags = hPkgs;
+          packageName = "grisette";
+        };
 
-        grisette = (hPkgs { ghcVersion = "9101"; ci = false; }).grisette;
+        grisette = (hPkgs {
+          ghcVersion = envLib.developmentGhcVersion;
+          ci = false;
+        }).grisette;
       in
       pkgs.lib.recursiveUpdate plainOutputs
         {
