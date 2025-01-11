9b107ba5cb284dc0481a5fc71c0d43d7602066c8
Sirui Lu <siruilu@cs.washington.edu>
2025-01-11 10:28:36 -0800

    :wrench: Update to use nix-build-env.out

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.grisette-nix-build-env.url = "github:lsrcz/grisette-nix-build-env/main";
-  inputs.pre-commit-hooks.url = "github:cachix/git-hooks.nix";
 
   outputs =
     {
@@ -11,139 +10,73 @@
       nixpkgs,
       flake-utils,
       grisette-nix-build-env,
-      ...
     }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
-      let
-        envLib = grisette-nix-build-env.lib.${system};
-
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            envLib.overlays.z3
-            envLib.overlays.hlintSrc
-            envLib.overlays.cvc5
-          ];
-        };
-
-        patchedHPkgs =
-          { ghcVersion }:
-          envLib.patchedHaskellPackages {
-            inherit pkgs;
-            ghcVersion = ghcVersion;
-          };
-
-        hPkgs =
-          { ghcVersion, ci }:
-          (patchedHPkgs { inherit ghcVersion; }).extend (
-            hfinal: hprev:
-            with envLib.haskellPackagesExtendHelpers {
-              inherit
-                pkgs
-                hfinal
-                hprev
-                ghcVersion
-                ;
-            }; {
-              grisette = envLib.setCIOptions {
-                inherit
-                  pkgs
-                  ghcVersion
-                  hfinal
-                  ci
-                  ;
-                extraTestToolDepends = byGhcVersion {
-                  "8107" = [ pkgs.z3 ];
-                  "902" = [ pkgs.z3 ];
-                  default = [
-                    pkgs.z3
-                    pkgs.bitwuzla
-                  ];
-                };
-                package = hfinal.callCabal2nix "grisette" ./. { };
-                mixDirs = [
-                  ""
-                  "spec/spec-tmp"
-                  "doctest/doctest-tmp"
+      grisette-nix-build-env.lib.${system}.output {
+        inherit nixpkgs system;
+        srcRoot = ./.;
+        extraHaskellPackages =
+          pkgs: hfinal: helpers: setCIOptions: with helpers; {
+            grisette = setCIOptions {
+              extraTestToolDepends = byGhcVersion {
+                "8107" = [ pkgs.z3 ];
+                "902" = [ pkgs.z3 ];
+                default = [
+                  pkgs.z3
+                  pkgs.bitwuzla
                 ];
               };
-            }
-          );
-
-        treefmt = envLib.treefmt {
-          inherit pkgs;
-          excludes = [
-            "tutorials/*.ipynb"
-            "tutorials/*.svg"
-          ];
-        };
-
-        pre-commit-check = envLib.pre-commit-check {
-          src = ./.;
-          inherit treefmt system;
-        };
-
-        devShellWithVersion =
-          {
-            ghcVersion,
-            isDevelopmentEnvironment ? false,
-            bitwuzla ? false,
-          }:
-          (envLib.devShell {
-            inherit pkgs isDevelopmentEnvironment;
-            haskellPackages = hPkgs {
-              inherit ghcVersion;
-              ci = false;
+              package = hfinal.callCabal2nix "grisette" ./. { };
+              mixDirs = [
+                ""
+                "spec/spec-tmp"
+                "doctest/doctest-tmp"
+              ];
             };
-            extraBuildInputs = [
-              pkgs.boolector
-              pkgs.cvc5
-            ] ++ (if bitwuzla then [ pkgs.bitwuzla ] else [ ]);
-          }).overrideAttrs
-            (old: {
-              shellHook = pre-commit-check.shellHook;
-            });
 
-        plainOutputs = envLib.defaultOutputs {
-          inherit pkgs devShellWithVersion;
-          haskellPackagesWithCiFlags = hPkgs;
-          packageName = "grisette";
-        };
-
-        grisette =
-          (hPkgs {
-            ghcVersion = envLib.developmentGhcVersion;
-            ci = false;
-          }).grisette;
-
-      in
-      pkgs.lib.recursiveUpdate plainOutputs {
-        formatter = treefmt;
-
-        checks = {
-          pre-commit-check = pre-commit-check;
-        };
-
-        devShells = {
-          "9101" = devShellWithVersion {
-            ghcVersion = "9101";
-            isDevelopmentEnvironment = true;
-            bitwuzla = true;
           };
-          default = devShellWithVersion {
-            ghcVersion = "9101";
-            isDevelopmentEnvironment = true;
-            bitwuzla = true;
-          };
-          "9121" = devShellWithVersion {
-            ghcVersion = "9121";
-            bitwuzla = true;
+        devShellExtraBuildInputs =
+          pkgs:
+          {
+            bitwuzla ? false,
+            ...
+          }:
+          [
+            pkgs.boolector
+            pkgs.cvc5
+          ]
+          ++ (if bitwuzla then [ pkgs.bitwuzla ] else [ ]);
+        treefmtExcludes = [
+          "tutorials/*.ipynb"
+          "tutorials/*.svg"
+        ];
+        pname = "grisette";
+        extraOutputs = pkgs: haskellPackages: devShellWithVersion: {
+          devShells = {
+            "9101" = devShellWithVersion {
+              ghcVersion = "9101";
+              config = {
+                isDevelopmentEnvironment = true;
+                bitwuzla = true;
+              };
+            };
+            default = devShellWithVersion {
+              ghcVersion = "9101";
+              config = {
+                isDevelopmentEnvironment = true;
+                bitwuzla = true;
+              };
+            };
+            "9121" = devShellWithVersion {
+              ghcVersion = "9121";
+              config = {
+                isDevelopmentEnvironment = false;
+                bitwuzla = true;
+              };
+            };
           };
         };
-
-        packages.grisette = grisette;
-        packages.default = grisette;
       }
     );
 }
